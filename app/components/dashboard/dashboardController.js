bdef36da8e81a4b073f0afc35c021961fa2034fc
Ptimagos <david.lepoultier@gmail.com>
2016-02-16 23:57:25 +0100

    Update main dashboard

--- app/components/dashboard/dashboardController.js
+++ app/components/dashboard/dashboardController.js
@@ -1,7 +1,91 @@
 angular.module('dashboard', [])
-.controller('DashboardController', ['$scope', 
-  function ($scope) {
+.controller('DashboardController', ['$scope', '$routeParams', 'ConsulNodes', 'ConsulContainers', 
+  'ConsulSwarmManager', 'ConsulSwarmAgent', 'SettingsConsul', 'Messages',
+  function ($scope, $routeParams, ConsulNodes, ConsulContainers, ConsulSwarmManager, ConsulSwarmAgent, SettingsConsul, Messages) {
     $scope.predicate = '-Created';
-    $scope.containers = [];
+    $scope.consulNodes = [];
+    $scope.consulContainers = [];
+    $scope.consulSwarmManager = [];
+    $scope.consulSwarmAgent = [];
+
+    ConsulNodes.query({recurse: 1}, function (d) {
+      $scope.consulNodes.total = d.length;
+      $scope.consulNodes.running = 0;
+      $scope.consulNodes.stopped = 0;
+      var stopped = 0;
+      for (var i = 0; i < d.length; i++) {
+        var item = d[i];  
+        var values = [];
+        values = JSON.parse(atob(item.Value));
+        switch(values.status){
+          case 'Healthy':
+            $scope.consulNodes.running++;
+            break;
+          default:
+            $scope.consulNodes.stopped++;
+            break;
+        }
+      }
+    });
+
+    ConsulContainers.query({recurse: 1}, function (d) {
+      $scope.consulContainers.total = d.length;
+      $scope.consulContainers.running = 0;
+      $scope.consulContainers.stopped = 0;
+      $scope.consulContainers.ghost = 0;
+      var stopped = 0;
+      for (var i = 0; i < d.length; i++) {
+        var item = d[i];  
+        var values = [];
+        values = JSON.parse(atob(item.Value));
+        if (values.status === 'Ghost') {
+          $scope.consulContainers.ghost++;
+        } else if (values.status.indexOf('Exit') !== -1 && values.status !== 'Exit 0') {
+          $scope.consulContainers.stopped++;
+        } else {
+          $scope.consulContainers.running++;
+        }
+      }
+    });
+
+    ConsulSwarmManager.query({recurse: 1}, function (d) {
+      $scope.consulSwarmManager.total = d.length;
+      $scope.consulSwarmManager.running = 0;
+      $scope.consulSwarmManager.stopped = 0;
+      var stopped = 0;
+      for (var i = 0; i < d.length; i++) {
+        var item = d[i];  
+        var values = [];
+        values = JSON.parse(atob(item.Value));
+        switch(values.status){
+          case 'Up':
+            $scope.consulSwarmManager.running++;
+            break;
+          default:
+            $scope.consulSwarmManager.stopped++;
+            break;
+        }
+      }
+    });
+
+    ConsulSwarmAgent.query({recurse: 1}, function (d) {
+      $scope.consulSwarmAgent.total = d.length;
+      $scope.consulSwarmAgent.running = 0;
+      $scope.consulSwarmAgent.stopped = 0;
+      var stopped = 0;
+      for (var i = 0; i < d.length; i++) {
+        var item = d[i];  
+        var values = [];
+        values = JSON.parse(atob(item.Value));
+        switch(values.status){
+          case 'Up':
+            $scope.consulSwarmAgent.running++;
+            break;
+          default:
+            $scope.consulSwarmAgent.stopped++;
+            break;
+        }
+      }
+    });
 
   }]);
