6d4e6b755a2c56369dcb9118ea8609d0dd2c5639
David Lepoultier <david.lepoultier@orange.com>
2016-03-08 18:24:02 +0100

    Merge all swarmui binary

--- src/swarmui/swarmui.go
+++ src/swarmui/swarmui.go
@@ -1,15 +1,23 @@
 package main 
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"io"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"fmt"
 )
+
+func msg() {
+	fmt.Printf("You must specified starting mode !\n")
+}
 
 type ConsulHandler struct {
 	path string
@@ -78,24 +86,185 @@ func createHandler(dir string, e string, e2 string, e3 string, e4 string) http.H
 	return mux
 }
 
+func sendHttp(w http.ResponseWriter, r *http.Request, client *http.Client) {
+
+    req, err := http.NewRequest(r.Method, "http:/" + r.URL.Path + "?" + r.URL.RawQuery, r.Body)
+    if err != nil {
+        log.Println(err)
+    }       
+    req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+        resp, err := client.Do(req)
+        if err != nil {
+            log.Println(err)
+        }       
+    defer resp.Body.Close()
+
+    copyHeader(w.Header(), resp.Header)
+    if _, err := io.Copy(w, resp.Body); err != nil {
+        log.Println(err)
+    }
+}
+
+func sendHttps(w http.ResponseWriter, r *http.Request, client *http.Client) {
+
+    req, err := http.NewRequest(r.Method, "https:/" + r.URL.Path + "?" + r.URL.RawQuery, r.Body)
+    if err != nil {
+        log.Println(err)
+    }       
+    req.Header.Set("X-Custom-Header", "myvalue")
+    req.Header.Set("Content-Type", "application/json")
+
+    resp, err := client.Do(req)
+    if err != nil {
+        log.Println(err)
+    }       
+    defer resp.Body.Close()
+
+    copyHeader(w.Header(), resp.Header)
+    if _, err := io.Copy(w, resp.Body); err != nil {
+        log.Println(err)
+    }
+}
+
+func swarmui() {
+	if len(os.Args) > 2 {
+		consul := os.Args[2]
+
+		var (
+			endpoint = flag.String("e", consul, "Consul endpoint")
+			endpoint2 = flag.String("e2", "http://localhost:9001", "Dockers endpoint")
+			endpoint3 = flag.String("e3", "http://localhost:9002", "Dockers TLS endpoint")
+			endpoint4 = flag.String("e4", "http://localhost:9003", "Dockers REPO endpoint")
+			addr     = flag.String("p", ":9000", "Address and port to serve swarmui")
+			assets   = flag.String("a", ".", "Path to the assets")
+		)
+
+		handler := createHandler(*assets, *endpoint, *endpoint2, *endpoint3, *endpoint4)
+		if err := http.ListenAndServe(*addr, handler); err != nil {
+			log.Println(handler)
+			log.Fatal(err)
+		}
+	} else {
+		msg()
+	}
+
+}
+
+func docker() {
+    tr := &http.Transport{
+        DisableCompression: true,
+        DisableKeepAlives: false,
+    }
+    client := &http.Client{Transport: tr}
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        sendHttp(w, r, client)
+    })
+    log.Fatal(http.ListenAndServe(":9001", nil))
+}
+
+func dockerRepo() {
+	if len(os.Args) > 2 {
+    	myProxy := os.Args[2]
+	    url_i := url.URL{}
+	    url_proxy, _ := url_i.Parse(myProxy)
+
+	    tr := &http.Transport{
+	        DisableCompression: false,
+	        DisableKeepAlives: false,
+	        Proxy: http.ProxyURL(url_proxy),
+	    }
+	    client := &http.Client{Transport: tr}
+
+	    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		    sendHttps(w, r, client)
+		})
+		log.Fatal(http.ListenAndServe(":9003", nil))
+	} else {
+		tr := &http.Transport{
+	        DisableCompression: false,
+	        DisableKeepAlives: false,
+	    }
+	    client := &http.Client{Transport: tr}
+
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		    sendHttps(w, r, client)
+		})
+		log.Fatal(http.ListenAndServe(":9003", nil))
+	}
+}
+
+func dockerTls() {
+    var (
+        certFile = flag.String("cert", "certs/cert.pem", "A PEM eoncoded certificate file.")
+        keyFile  = flag.String("key", "certs/key.pem", "A PEM encoded private key file.")
+        caFile   = flag.String("CA", "certs/ca.pem", "A PEM eoncoded CA's certificate file.")
+    )
+
+    // Load client cert
+    cert, err := tls.LoadX509KeyPair(*certFile, *keyFile)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // Load CA cert
+    caCert, err := ioutil.ReadFile(*caFile)
+    if err != nil {
+        log.Fatal(err)
+    }
+    caCertPool := x509.NewCertPool()
+    caCertPool.AppendCertsFromPEM(caCert)
+
+    // Setup HTTPS client
+    tlsConfig := &tls.Config{
+        Certificates: []tls.Certificate{cert},
+        RootCAs:      caCertPool,
+    }
+    tlsConfig.BuildNameToCertificate()
+
+    tr := &http.Transport{
+        DisableCompression: false,
+        DisableKeepAlives: false,
+        TLSClientConfig: tlsConfig,
+    }
+    client := &http.Client{Transport: tr}
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        sendHttps(w, r, client)
+    })
+    log.Fatal(http.ListenAndServe(":9002", nil))
+
+}
+
 func main() {
 
-	consul := os.Args[1]
+	if len(os.Args) > 1 {
+		starting := os.Args[1]
 
-	var (
-		endpoint = flag.String("e", consul, "Consul endpoint")
-		endpoint2 = flag.String("e2", "http://localhost:9001", "Dockers endpoint")
-		endpoint3 = flag.String("e3", "http://localhost:9002", "Dockers TLS endpoint")
-		endpoint4 = flag.String("e4", "http://localhost:9003", "Dockers REPO endpoint")
-		addr     = flag.String("p", ":9000", "Address and port to serve dockerui")
-		assets   = flag.String("a", ".", "Path to the assets")
-	)
+		flag.Parse()
 
-	flag.Parse()
+		switch starting {
+			case "swarmui":
+				fmt.Printf("Starting %s server\n", starting)
+				swarmui()
+				
+			case "docker":
+				fmt.Printf("Starting Dockers endpoint\n")
+				docker()
 
-	handler := createHandler(*assets, *endpoint, *endpoint2, *endpoint3, *endpoint4)
-	if err := http.ListenAndServe(*addr, handler); err != nil {
-		log.Println(handler)
-		log.Fatal(err)
+			case "dockerTls":
+				fmt.Printf("Starting Dockers Tls endpoint\n")
+				dockerTls()
+
+			case "dockerRepo":
+				fmt.Printf("Starting Dockers Repo endpoint\n")
+				dockerRepo()
+
+			default:
+				msg()
+		}
+	} else {
+		msg()
 	}
 }
